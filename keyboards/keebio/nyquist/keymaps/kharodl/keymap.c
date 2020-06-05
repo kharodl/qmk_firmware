b79efa18140337be0f31f2fe6803db7b6822d208
Lovejit <kharodl@users.noreply.github.com>
2020-06-04 17:41:12 -0700

    forgot a comma

--- keyboards/keebio/nyquist/keymaps/kharodl/keymap.c
+++ keyboards/keebio/nyquist/keymaps/kharodl/keymap.c
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_PLOVER] = LAYOUT_ortho_4x12(
-    PLV_EXT, KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1
+    PLV_EXT, KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,
     PLV_CTL, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     PLV_STP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     PLV_EXT, _______, _______, KC_C,    KC_V,    _______, _______, KC_N,    KC_M,    _______, _______, PLV_EXT
